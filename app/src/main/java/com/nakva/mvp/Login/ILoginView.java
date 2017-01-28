7351153fcdf55b72d3a54597f9143fa661fa1b53
Juancho <linanjm90@gmail.com>
2017-01-28 16:35:47 -0600

    new project

---
+++ app/src/main/java/com/nakva/mvp/Login/ILoginView.java
@@ -0,0 +1,10 @@
+package com.nakva.mvp.Login;
+
+/**
+ * Created by Juancho on 28/01/17.
+ * Nakva
+ * linanjm90@gmail.com
+ */
+
+public interface ILoginView {
+}
---
+++ app/src/test/java/com/nakva/mvp/Login/LoginPresenterTest.java
@@ -0,0 +1,12 @@
+package com.nakva.mvp.Login;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Juancho on 28/01/17.
+ * Nakva
+ * linanjm90@gmail.com
+ */
+public class LoginPresenterTest {
+
+}
